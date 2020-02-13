0abf6af9edbc5fd0cf2a6bf67e841e3c9fb26cc5
Mary <marykmachado@gmail.com>
2020-02-12 23:10:19 -0500

    Task: Fixing test_Calculator.py

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -10,12 +10,6 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_subtraction(self):
-        test_data = CsvReader("Tests/Data/subtraction.csv").data
-        for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
-            self.assertEqual(self.calculator.result, result)
 
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
