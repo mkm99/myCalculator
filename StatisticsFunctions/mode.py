cffb0010e5c0502c5d1616f796d0257f6d405650
Mary <marykmachado@gmail.com>
2020-02-28 01:52:01 -0500

    Fix: mode.py

--- StatisticsFunctions/mode.py
+++ StatisticsFunctions/mode.py
@@ -1,6 +1,8 @@
-import statistics as stats
+from statistics import mode
 
 class Mode():
     @staticmethod
     def mode(data):
-        return stats.mode(data)
+        return mode(data)
+
+#print(Mode.mode([1,2,3,4,3,2,1,2,33]))
