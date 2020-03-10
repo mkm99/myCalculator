ff7d7facac404263715ff4a4eafbe7234a66f32c
Mary <marykmachado@gmail.com>
2020-03-10 15:09:07 -0400

    Fix: fixing file path

--- Tests/test_CSVTests.py
+++ Tests/test_CSVTests.py
@@ -5,7 +5,7 @@ from pprint import pprint
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.csv_reader = CsvReader('/Data/employee_birthday.csv')
+        self.csv_reader = CsvReader('Tests/Data/employee_birthday.csv')
 
     def test_return_data_as_objects(self):
         people = self.csv_reader.return_data_as_class('person')
